6d450c8d7cdb2dc2ffdefd0c24cfb9cf589ac328
INexizI <39536903+INexizI@users.noreply.github.com>
2022-06-30 18:09:12 +0300

    upd ban_controller

--- app/javascript/controllers/ban_controller.js
+++ app/javascript/controllers/ban_controller.js
@@ -2,21 +2,27 @@ import { Controller } from '@hotwired/stimulus'
 
 export default class extends Controller {
   userinfo() {
-    // const css = {
-    //   'display': 'flex',
-    //   'justify-content': 'center',
-    //   'align-items': 'center',
-    //   'font-size': '15px'
-    // }
-    // $(this.element).parent().next().fadeToggle('fast').css(css)
-    // $(this.element).parent().append(`
-    //   <div id="user-info">
-    //     <span id="to600">10m</span>
-    //     <span id="to3600">1h</span>
-    //     <span id="to86400">1d</span>
-    //     <span id="to604800">1w</span>
-    //     <span><img src="/images/slash.svg" id="ch-badge"></span>
-    //   </div>
-    // `)
+    let info = `<div id="user-info">
+                  <p>
+                    <span id="to600" data-controller="ban", data-action="click->ban#timeout">10m</span>
+                    <span id="to3600" data-controller="ban", data-action="click->ban#timeout">1h</span>
+                    <span id="to86400" data-controller="ban", data-action="click->ban#timeout">1d</span>
+                    <span id="to604800" data-controller="ban", data-action="click->ban#timeout">1w</span>
+                    <span id="toP" data-controller="ban", data-action="click->ban#timeout">
+                      <img src="/images/slash.svg" id="ch-badge">
+                    </span>
+                  </p>
+                  <span id="close" data-controller="ban", data-action="click->ban#close">✕</span>
+                </div>`
+
+    $('#user-info').length == 0 ? $(this.element).parent().append(info) : $('#user-info').remove()
+  }
+
+  close() {
+    $('#user-info').remove()
+  }
+
+  timeout() {
+    let duration = ($(this.element).attr('id')).slice(2)
   }
 }
